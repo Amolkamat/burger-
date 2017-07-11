36b7bdeafb9818e413a429d4290161365ce273df
Amol Kamatmhamai <Amol Kamatmhamai>
2017-07-11 12:04:45 -0400

    Pushed all files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('BLT');
+INSERT INTO burgers (burger_name) VALUES ('Fish Fillet');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Parmesan');
+INSERT INTO burgers (burger_name) VALUES ('Cheesesteak');
